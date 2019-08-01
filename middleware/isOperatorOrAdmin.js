176016066113d147ead18aae9def4119dd7781d5
sarvar883 <sarvarik883@gmail.com>
2019-08-01 12:13:50 +0500

    make it better

--- middleware/isOperatorOrAdmin.js
+++ middleware/isOperatorOrAdmin.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  if (req.user.occupation !== 'operator' || req.user.occupation !== 'admin') {
+  if (req.user.occupation !== 'operator' && req.user.occupation !== 'admin') {
     return res.status(401).send({ error: 'You are not an Operator or Admin' });
   }
   next();
