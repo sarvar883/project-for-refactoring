1a2e9c52a6c260e110256359fc26378028a18bbb
sarvar883 <sarvarik883@gmail.com>
2019-12-25 12:33:24 +0500

    make it better

---
+++ client/public/test.js
@@ -0,0 +1,9 @@
+var OneSignal = window.OneSignal || [];
+OneSignal.push(function () {
+  OneSignal.init({
+    appId: "c2644c46-99cd-4de0-8d7d-ba1bac69b23a",
+    notifyButton: {
+      enable: true,
+    },
+  });
+});
