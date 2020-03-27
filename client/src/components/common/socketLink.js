91533cd9a7d605da012fb6038202e4152d92cd16
sarvar883 <sarvarik883@gmail.com>
2020-03-27 19:48:36 +0500

    make it better

--- client/src/components/common/socketLink.js
+++ client/src/components/common/socketLink.js
@@ -1,4 +1,4 @@
-const socketLink = 'http://localhost:5000';
-// const socketLink = 'https://fierce-scrubland-41952.herokuapp.com';
+// const socketLink = 'http://localhost:5000';
+const socketLink = 'https://fierce-scrubland-41952.herokuapp.com';
 
 module.exports = socketLink;
